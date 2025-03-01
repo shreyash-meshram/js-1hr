9eb77042a4ea93f9a1b6de04d4212365a3658cbc
shreyash-meshram <shreyashinno@gmail.com>
2025-03-01 05:22:35 +0000

    var and datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accId = 14321
+let accEmail = "shrey@gmail.com"
+var accPassword = "10101"
+let accCity = "Bhandara"
+
+let accState;
+
+accCity = "Nagpur"
+
+// accId = 32321
+
+console.log(accId)
+console.log(accEmail)
+console.log(accPassword)
+console.log(accCity)
+console.table([accId, accEmail, accPassword, accCity, accState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) ---> we are using nodeJs not browser
+
+Note :- "An Original documentation of javascript is <tc39.es>"
+console.log(3+3); 
+
+let name = "Shreyash"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// null => standalone value
+//undefined
+// symbol => unique
+
+// object
+
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
