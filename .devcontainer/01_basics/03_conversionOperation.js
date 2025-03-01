f536861265204b7810497552aff3c2d72ebe75d6
shreyash-meshram <shreyashinno@gmail.com>
2025-03-01 05:42:54 +0000

    conversion operations

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = 33
+let score2 = "33"
+
+// console.log(typeof score)
+// console.log(typeof score2)
+
+console.log(typeof (score))
+console.log(typeof (score2))
+
+let changed = Number(score2)
+console.log(typeof changed)
+
+
